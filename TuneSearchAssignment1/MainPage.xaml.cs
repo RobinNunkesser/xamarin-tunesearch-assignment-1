4ed2ea4723febe69d9d3a630c8b1f399c3909f8b
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-14 15:51:39 +0100

    Initial commit

---
+++ TuneSearchAssignment1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExplicitArchitecture;
+using ExplicitArchitecture.TuneSearchExample.Core.Ports;
+using ExplicitArchitecture.TuneSearchExample.MockCore;
+using Xamarin.Forms;
+
+namespace TuneSearchAssignment1
+{
+    public partial class MainPage : ContentPage
+    {
+        private IService<ISearchTerm, List<ICollectionEntity>> _service = new MockSearchTracksService();
+
+        public MainPage()
+        {
+            InitializeComponent();                        
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await _service.Execute(new SearchTermDTO { Term = "Term" }, SuccessHandler, ErrorHandler);
+        }
+
+        private void ErrorHandler(Exception obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SuccessHandler(List<ICollectionEntity> obj)
+        {
+            Debug.WriteLine(obj.ToString());
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TuneSearchAssignment1/SearchTermDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExplicitArchitecture.TuneSearchExample.Core.Ports;
+
+namespace TuneSearchAssignment1
+{
+    public class SearchTermDTO : ISearchTerm
+    {
+        public SearchTermDTO()
+        {
+        }
+
+        public string Term { get; set; }
+    }
+}
